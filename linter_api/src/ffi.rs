f6d1b10bf9ee4af1d3350fb609c66293896f96c5
xFrednet <xFrednet@gmail.com>
2022-09-16 12:47:49 +0200

    Add `FfiSlice`

--- linter_api/src/ffi.rs
+++ linter_api/src/ffi.rs
@@ -29,7 +29,7 @@ impl<'a> From<&'a str> for Str<'a> {
     }
 }
 
-impl<'a> From<&Str<'a>> for &str {
+impl<'a> From<&Str<'a>> for &'a str {
     fn from(src: &Str<'a>) -> Self {
         unsafe {
             let data = slice::from_raw_parts(src.data, src.len);
@@ -70,5 +70,41 @@ impl<T> From<Option<T>> for FfiOption<T> {
             Option::Some(t) => FfiOption::Some(t),
             Option::None => FfiOption::None,
         }
+    }
+}
+
+#[repr(C)]
+pub struct FfiSlice<'a, T> {
+    _lifetime: PhantomData<&'a ()>,
+    data: *const T,
+    len: usize,
+}
+
+impl<'a, T> FfiSlice<'a, T> {
+    pub fn as_slice(&self) -> &'a [T] {
+        self.into()
+    }
+}
+
+impl<'a, T> From<&'a [T]> for FfiSlice<'a, T> {
+    fn from(src_data: &'a [T]) -> Self {
+        Self {
+            _lifetime: PhantomData,
+            data: src_data.as_ptr(),
+            len: src_data.len(),
+        }
+    }
+}
+
+impl<'a, T> From<&FfiSlice<'a, T>> for &'a [T] {
+    fn from(src: &FfiSlice<'a, T>) -> Self {
+        unsafe { slice::from_raw_parts(src.data, src.len) }
+    }
+}
+
+impl<'a, T: std::fmt::Debug> std::fmt::Debug for FfiSlice<'a, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let data: &[T] = self.into();
+        f.debug_list().entries(data.iter()).finish()
     }
 }
