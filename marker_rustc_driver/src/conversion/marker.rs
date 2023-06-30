104ed9fa1a29907b94f932752c5172ca163665f7
xFrednet <xFrednet@gmail.com>
2023-06-30 15:22:07 +0200

    Adapter: Use new utils visitor, to visit everything

--- marker_rustc_driver/src/conversion/marker.rs
+++ marker_rustc_driver/src/conversion/marker.rs
@@ -167,6 +167,7 @@ impl<'ast, 'tcx> MarkerConverterInner<'ast, 'tcx> {
             (hir::LangItem::FormatArguments, self.to_symbol_id(Symbol::intern("FormatArguments"))),
             (hir::LangItem::FormatArgument, self.to_symbol_id(Symbol::intern("FormatArgument"))),
             (hir::LangItem::TryTraitBranch, self.to_symbol_id(Symbol::intern("Try::branch"))),
+            (hir::LangItem::FormatPlaceholder, self.to_symbol_id(Symbol::intern(""))),
         ];
 
         self.lang_item_map.borrow_mut().extend(list);
