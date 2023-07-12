94a934b2e47f2e195736aafaf7b3094a240bc6b9
xFrednet <xFrednet@gmail.com>
2023-07-13 00:31:13 +0200

    ui-lints: Migrate to `marker_uitest`

--- marker_uilints/tests/uitest.rs
+++ marker_uilints/tests/uitest.rs
@@ -1,33 +1,10 @@
-use std::{
-    collections::HashMap,
-    env, fs,
-    num::NonZeroUsize,
-    path::{Path, PathBuf},
-    process::Command,
-};
-use ui_test::*;
+use marker_uitest::ui_test::*;
+use std::{env, path::Path};
 
-#[test]
-fn ui_test() -> ui_test::color_eyre::Result<()> {
-    let path = "../target";
+fn main() -> color_eyre::Result<()> {
+    let mut config = marker_uitest::simple_ui_test_config!("tests/ui", "../target")?;
 
-    let setup = run_test_setup();
-    for (key, val) in setup.env_vars {
-        env::set_var(key, val);
-    }
-
-    let mut config = Config {
-        mode: Mode::Yolo,
-        output_conflict_handling: OutputConflictHandling::Error("BLESS=1 cargo uitest".into()),
-        dependencies_crate_manifest_path: Some("./Cargo.toml".into()),
-        num_test_threads: NonZeroUsize::new(1).unwrap(),
-        ..Config::rustc("tests/ui".into())
-    };
-
-    config.program.program = PathBuf::from(setup.rustc_path);
-    config.program.args.push("-Aunused".into());
-
-    let bless = std::env::var("BLESS").eq(&Ok("1".to_string()));
+    let bless = env::var_os("BLESS").is_some() || env::args().any(|arg| arg == "--bless");
     if bless {
         config.output_conflict_handling = OutputConflictHandling::Bless
     }
@@ -40,12 +17,6 @@ fn ui_test() -> ui_test::color_eyre::Result<()> {
         config.stderr_filter(pat, repl);
         config.stdout_filter(pat, repl);
     }
-
-    // hide binaries generated for successfully passing tests
-    let tmp_dir = tempfile::tempdir_in(path)?;
-    let tmp_dir = tmp_dir.path();
-    config.out_dir = tmp_dir.into();
-    config.path_stderr_filter(tmp_dir, "$TMP");
 
     let test_name_filter = test_name_filter();
     run_tests_generic(
@@ -68,56 +39,5 @@ fn test_name_filter() -> Box<dyn Fn(&Path) -> bool + Sync> {
         })
     } else {
         Box::new(|_| true)
-    }
-}
-
-struct TestSetup {
-    rustc_path: String,
-    /// The environment values that should be set. The first element is the
-    /// value name, the second is the value the it should be set to.
-    env_vars: HashMap<String, String>,
-}
-
-/// This function calls `cargo-marker` for the basic test setup. For normal linting
-/// crates this will need to be adjusted to run the installed `cargo-marker` version
-///
-/// In the future it would be nice to have a nice wrapper library as well.
-fn run_test_setup() -> TestSetup {
-    const CARGO_MARKER_INVOCATION: &[&str] = &["run", "--bin", "cargo-marker", "--features", "dev-build", "--"];
-
-    // ../marker/marker_uitest
-    let current_dir = env::current_dir().unwrap();
-    let lint_crate_src = fs::canonicalize(&current_dir).unwrap();
-    let lint_spec = format!(
-        r#"{} = {{ path = "{}" }}"#,
-        env!("CARGO_PKG_NAME"),
-        lint_crate_src.display()
-    );
-    let mut cmd = Command::new("cargo");
-    let output = cmd
-        .current_dir(current_dir.parent().unwrap())
-        .args(CARGO_MARKER_INVOCATION)
-        .arg("-l")
-        .arg(lint_spec)
-        .arg("--test-setup")
-        .output()
-        .expect("Unable to run the test setup using `cargo-marker`");
-    let stdout = String::from_utf8(output.stdout).unwrap();
-
-    if !output.status.success() {
-        let stderr = String::from_utf8(output.stderr).unwrap();
-        panic!("Test setup failed:\n\n===STDOUT===\n{stdout}\n\n===STDERR===\n{stderr}\n");
-    }
-
-    let mut env_vars: HashMap<_, _> = stdout
-        .lines()
-        .filter_map(|line| line.strip_prefix("env:"))
-        .filter_map(|line| line.split_once('='))
-        .map(|(var, val)| (var.to_string(), val.to_string()))
-        .collect();
-
-    TestSetup {
-        rustc_path: env_vars.remove("RUSTC_WORKSPACE_WRAPPER").unwrap(),
-        env_vars,
     }
 }
