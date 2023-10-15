8ab924030b95c3e9c528864ebdb4e3c105553cd3
Samuel Moelius <sam@moeli.us>
2023-10-15 18:33:12 -0400

    Address review comments

--- marker_rustc_driver/src/main.rs
+++ marker_rustc_driver/src/main.rs
@@ -1,3 +1,6 @@
+//! This is the source file for the `rustc_marker_driver` binary. However, the bulk of its logic is
+//! in `../lib.rs`.
+
 #![doc = include_str!("../../README.md")]
 #![feature(rustc_private)]
 #![feature(lint_reasons)]
