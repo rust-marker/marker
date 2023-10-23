ccfcce10b39483df4919748dd17887631ac7f893
Vitalii Kryvenko <gersoh3@gmail.com>
2023-10-23 16:25:25 +0300

    Fix archvive layout for marker_rustc_driver on ARM (#299)
    The layout of `marker_rustc_driver-aarch64-unknown-linux-gnu.tar.gz` and `zip` turns out to differ from the layout of all other artifacts. This is because this version of driver is compiled separately using a custom quemu setup, packaging and upload.

--- scripts/release/upload.sh
+++ scripts/release/upload.sh
@@ -6,17 +6,23 @@ script_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
 . $script_dir/../lib.sh
 
+# The tag of the release that will be updated with the new artifact
 tag="$1"
+
+# Path to the file on the local file system that will be uploaded to GitHub
 file="$2"
 
-with_log tar --auto-compress --create --file "$file.tar.gz" "$file"
-with_log zip -r $file.zip $file
+# The name of the artifact that will be uploaded to GitHub
+artifact="$3"
 
-with_log shasum -a 256 "$file.tar.gz" "$file.zip" > "$file.sha256"
+with_log tar --auto-compress --create --file "$artifact.tar.gz" "$file"
+with_log zip -r $artifact.zip $file
+
+with_log shasum -a 256 "$artifact.tar.gz" "$artifact.zip" > "$artifact.sha256"
 
 with_backoff gh release upload \
     "$tag" \
-    "$file.tar.gz" \
-    "$file.zip" \
-    "$file.sha256" \
+    "$artifact.tar.gz" \
+    "$artifact.zip" \
+    "$artifact.sha256" \
     --clobber
